fd7c38ea0176f5e7047c5bc6b17b1f9043e2bba5
Anna <annfr542@student.liu.se>
2020-07-09 15:27:55 +0200

    Updates

---
+++ precache-manifest.75a4d33460f527fadcf4886a2757c3ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc71d45280dc39ff954e3d4e7faa8c88",
+    "url": "/dashboardTemplate/index.html"
+  },
+  {
+    "revision": "66cb47cdaf2a35e77339",
+    "url": "/dashboardTemplate/static/css/2.cac4bd09.chunk.css"
+  },
+  {
+    "revision": "a52f8a2a302b5e544aa1",
+    "url": "/dashboardTemplate/static/css/main.01b61fb4.chunk.css"
+  },
+  {
+    "revision": "66cb47cdaf2a35e77339",
+    "url": "/dashboardTemplate/static/js/2.a426caec.chunk.js"
+  },
+  {
+    "revision": "0f1dcfdc34a9851ce124c5850e083ebb",
+    "url": "/dashboardTemplate/static/js/2.a426caec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52f8a2a302b5e544aa1",
+    "url": "/dashboardTemplate/static/js/main.a1a67e8f.chunk.js"
+  },
+  {
+    "revision": "71ec57df786ca54f160c",
+    "url": "/dashboardTemplate/static/js/runtime-main.51ef6295.js"
+  },
+  {
+    "revision": "c13fa0899ff1182ab56946d9f8d2463f",
+    "url": "/dashboardTemplate/static/media/AOI.c13fa089.zip"
+  },
+  {
+    "revision": "f4ccc24c6654b1c593c1ec0e95aee37a",
+    "url": "/dashboardTemplate/static/media/Flood.f4ccc24c.zip"
+  },
+  {
+    "revision": "f8041b0394cc02fa9256be370287c198",
+    "url": "/dashboardTemplate/static/media/Watersheds.f8041b03.zip"
+  }
+]);
